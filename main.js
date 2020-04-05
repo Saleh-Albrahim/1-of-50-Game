1d5c978b3f3d5a19e2b2cc14b57ad2294415fe5f
Saleh Alibrahim <s.albrahim.com>
2020-04-05 22:29:12 +0300

    fix bug in the timer

--- main.js
+++ main.js
@@ -1,79 +1,84 @@
-let counter=0;
-let startTime = Math.floor(Date.now() / 1000);
-let boxs=document.querySelectorAll(".box");
-let numberTo25=[];
-let numberTo50=[];
-let firstbox=true;
-let faild=false;
-let win=false;
+let counter = 0;
+let startTime = 0;
+let boxs = document.querySelectorAll(".box");
+let numberTo25 = [];
+let numberTo50 = [];
+let firstbox = true;
+let faild = false;
+let win = false;
+
+
 for (let index = 1; index <= 25; index++) {
-    numberTo25.push(index);   
+    numberTo25.push(index);
 }
 for (let index = 26; index <= 50; index++) {
-    numberTo50.push(index);   
-}
-console.log(numberTo25);
-for (let i = numberTo25.length-1;i>=0;i--) {
-    let randomItem = numberTo25.splice(Math.floor(Math.random()*numberTo25.length), 1);
-    boxs[i].setAttribute("id",`${i}`)
-    boxs[i].innerHTML=randomItem;
+    numberTo50.push(index);
 }
 
 
 
 
-function checkResult(id,value){
-    if(firstbox){
+for (let i = 0; i < 25; i++) {
+    let randomItem = numberTo25.splice(Math.floor(Math.random() * numberTo25.length), 1);
+    boxs[i].setAttribute("id", `${i}`)
+    boxs[i].innerHTML = randomItem;
+}
+
+
+
+
+function checkResult(id, value) {
+    if (firstbox) {
         startTime = Math.floor(Date.now() / 1000);
         startTimeCounter();
-        firstbox=false;
+        firstbox = false;
     }
-   
-    if(value!=counter+1){
-        faild=true;
+
+    if (value != counter + 1) {
+        faild = true;
         alert("You Faild");
 
     }
-    else{
+    else {
         counter++;
-        let randomItem = numberTo50.splice(Math.floor(Math.random()*numberTo50.length), 1); 
-       
-        if(randomItem.length==0){
-            randomItem="@";
-        }
-        document.getElementById(`${id}`).innerHTML=randomItem;
-        let score=parseInt(document.querySelector("#score").innerHTML);
-        score++;
-        
-        document.querySelector("#score").innerHTML=score;
+        let randomItem = numberTo50.splice(Math.floor(Math.random() * numberTo50.length), 1);
 
-        if(score==50){
-             win=true;
+        if (randomItem.length == 0) {
+            randomItem = "@";
+        }
+        document.getElementById(`${id}`).innerHTML = randomItem;
+        let score = parseInt(document.querySelector("#score").innerHTML);
+        score++;
+
+        document.querySelector("#score").innerHTML = score;
+
+        if (score == 50) {
+            win = true;
             alert("You Won !");
         }
     }
-    }
+}
 
-    
-   
-    
-    function startTimeCounter() {
-        var now = Math.floor(Date.now() / 1000); // get the time now
-        var diff = now - startTime; // diff in seconds between now and start
-        var m = Math.floor(diff / 60); // get minutes value (quotient of diff)
-        var s = Math.floor(diff % 60); // get seconds value (remainder of diff)
-        m = checkTime(m); // add a leading zero if it's single digit
-        s = checkTime(s); // add a leading zero if it's single digit
-        document.getElementById("timer").innerHTML =  s; // update the element where the timer will appear
-        if(win==false&&faild==false){
-        var t = setTimeout(startTimeCounter, 500); // set a timeout to update the timer
-        }
+
+
+
+function startTimeCounter() {
+    var now = Math.floor(Date.now() / 1000); 
+    var diff = now - startTime; 
+    var m = Math.floor(diff / 60); 
+    var s = Math.floor(diff % 60); 
+    s = checkTime(s); 
+    m = checkTime(m); 
+    document.getElementById("timer").innerHTML = m + " : "+ s; 
+    if (win == false && faild == false) {
+        var t = setTimeout(startTimeCounter, 500); 
     }
-    
-    function checkTime(i) {
-        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-        return i;
-    }
-    
-  
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
+
+
 
