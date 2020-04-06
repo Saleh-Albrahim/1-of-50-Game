dc64b4fd416cf5fe95466858ad01210cb92007d2
Saleh Alibrahim <s.albrahim.com>
2020-04-06 09:04:11 +0300

    fix the restart button

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
-let counter = 0;
-let startTime = 0;
-let boxs = document.querySelectorAll(".box");
-let numberTo25 = [];
-let numberTo50 = [];
-let firstbox = true;
-let faild = false;
-let win = false;
+let counter;
+let startTime;
+let boxs;
+let numberTo25;
+let numberTo50;
+let firstbox;
+let faild;
+let win;
+let score;
+let restart;
+
+function startTheGame() {
+
+    counter = 0;
+    startTime = 0;
+    boxs = document.querySelectorAll(".box");
+    numberTo25 = [];
+    numberTo50 = [];
+    firstbox = true;
+    faild = false;
+    win = false;
 
 
-for (let index = 1; index <= 25; index++) {
-    numberTo25.push(index);
+    for (let index = 1; index <= 25; index++) {
+        numberTo25.push(index);
+    }
+    for (let index = 26; index <= 50; index++) {
+        numberTo50.push(index);
+    }
+
+
+
+
+    for (let i = 0; i < 25; i++) {
+        let randomItem = numberTo25.splice(Math.floor(Math.random() * numberTo25.length), 1);
+        boxs[i].setAttribute("id", `${i}`)
+        boxs[i].innerHTML = randomItem;
+    }
+
+
 }
-for (let index = 26; index <= 50; index++) {
-    numberTo50.push(index);
-}
-
-
-
-
-for (let i = 0; i < 25; i++) {
-    let randomItem = numberTo25.splice(Math.floor(Math.random() * numberTo25.length), 1);
-    boxs[i].setAttribute("id", `${i}`)
-    boxs[i].innerHTML = randomItem;
-}
-
-
-
 
 function checkResult(id, value) {
-    if (win||faild ) {
+    if (win || faild) {
         return;
     }
 
@@ -63,19 +76,20 @@ function checkResult(id, value) {
     }
 }
 
-
-
-
 function startTimeCounter() {
-    var now = Math.floor(Date.now() / 1000); 
-    var diff = now - startTime; 
-    var m = Math.floor(diff / 60); 
-    var s = Math.floor(diff % 60); 
-    s = checkTime(s); 
-    m = checkTime(m); 
-    document.getElementById("timer").innerHTML = m + " : "+ s; 
-    if (!win  && !faild) {
-        var t = setTimeout(startTimeCounter, 500); 
+    if(restart){
+        restart=false;
+        return;
+    }
+    var now = Math.floor(Date.now() / 1000);
+    var diff = now - startTime;
+    var m = Math.floor(diff / 60);
+    var s = Math.floor(diff % 60);
+    s = checkTime(s);
+    m = checkTime(m);
+    document.getElementById("timer").innerHTML = m + " : " + s;
+    if (!win && !faild ) {
+        setTimeout(startTimeCounter, 500);
     }
 }
 
@@ -83,6 +97,16 @@ function checkTime(i) {
     if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
     return i;
 }
+
+function Restart() {
+    restart=true;
+    startTheGame();
+    document.querySelector("#score").innerHTML="0";
+    document.getElementById("timer").innerHTML =  "00 : 00";
+
+
+}
+startTheGame();
 
 
 
